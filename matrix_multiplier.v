3e6906141bd52fea42a5b17b56fd350a64cfbec6
nimasalemahim <45598863+nimasalemahim@users.noreply.github.com>
2021-02-05 17:01:12 +0330

    New update matrix_mult

--- matrix_multiplier.v
+++ matrix_multiplier.v
@@ -53,15 +53,15 @@ localparam wait_for_add_s = 4;
 localparam out_is_ready_s = 5;
 
 reg [ELEMENT_LENGTH-1:0] out_matrix [1:NUM_FINAL_ROW][1:NUM_FINAL_COL];
-reg [ELEMENT_LENGTH-1:0] temp_matrix [1:NUM_FINAL_ROW][1:NUM_FINAL_COL];
-reg [ELEMENT_LENGTH-1:0] temp2_matrix [1:NUM_FINAL_ROW][1:NUM_FINAL_COL];
+wire [ELEMENT_LENGTH-1:0] temp_matrix [1:NUM_FINAL_ROW][1:NUM_FINAL_COL];
+wire [ELEMENT_LENGTH-1:0] temp2_matrix [1:NUM_FINAL_ROW][1:NUM_FINAL_COL];
 reg [FIRST_IN_LENGTH - 1 : 0] In1_Copy;
 reg [SECOND_IN_LENGTH - 1 : 0] In2_Copy;
 reg [SELECT_LENGTH - 1:0] select_signal;
 reg [ELEMENT_LENGTH * NUM_FIRST_COL - 1:0] first_rows [1:NUM_FIRST_ROW];
 reg [ELEMENT_LENGTH * NUM_SECOND_ROW - 1:0] second_cols [1:NUM_SECOND_COL];
-reg [ELEMENT_LENGTH - 1:0] out_rows_mux [1:NUM_FIRST_ROW];
-reg [ELEMENT_LENGTH - 1:0] out_cols_mux [1:NUM_SECOND_COL];
+wire [ELEMENT_LENGTH - 1:0] out_rows_mux [1:NUM_FIRST_ROW];
+wire [ELEMENT_LENGTH - 1:0] out_cols_mux [1:NUM_SECOND_COL];
 reg [2:0] state;
 
 // multiplier signals
@@ -69,9 +69,9 @@ reg rst_mult = 0;
 reg in1_stb = 0;
 reg in2_stb = 0;
 reg output_ack = 0;
-reg output_stb[1:NUM_FIRST_ROW][1:NUM_SECOND_COL];
-reg in1_ack[1:NUM_FIRST_ROW][1:NUM_SECOND_COL];
-reg in2_ack[1:NUM_FIRST_ROW][1:NUM_SECOND_COL];
+wire output_stb[1:NUM_FIRST_ROW][1:NUM_SECOND_COL];
+wire in1_ack[1:NUM_FIRST_ROW][1:NUM_SECOND_COL];
+wire in2_ack[1:NUM_FIRST_ROW][1:NUM_SECOND_COL];
 reg in1_full_ack = 1;
 reg in2_full_ack = 1;
 reg output_full_stb = 1;
@@ -80,22 +80,23 @@ reg output_full_stb = 1;
 reg reset_adder = 0;
 reg load_input = 0;
 reg res_ack = 0;
-reg res_ready[1:NUM_FIRST_ROW][1:NUM_SECOND_COL];
+wire res_ready[1:NUM_FIRST_ROW][1:NUM_SECOND_COL];
 reg res_full_ready = 1;
 
-integer k,t,g,l;
+integer k,t,g,l,n,m;
 genvar i,j;
+
 generate
     for(i=0; i<NUM_FINAL_ROW; i=i+1) begin
         for (j=0 ;j<NUM_FINAL_COL; j=j+1) begin
-            assign Out[OUT_LENGTH-1-ELEMENT_LENGTH*(i*NUM_FINAL_COL+j):-(ELEMENT_LENGTH-1)] = out_matrix[i+1][j+1];
+            assign Out[OUT_LENGTH-1-ELEMENT_LENGTH*(i*NUM_FINAL_COL+j):OUT_LENGTH-1-ELEMENT_LENGTH*(i*NUM_FINAL_COL+j)-(ELEMENT_LENGTH-1)] = out_matrix[i+1][j+1];
         end
     end
 endgenerate
 generate
-    for(i=0; i<NUM_FIRST_COL; i=i+1) begin
+    for(i=0; i<NUM_FIRST_ROW; i=i+1) begin
         always @* begin
-            first_rows[i+1] = In1_Copy[FIRST_IN_LENGTH - 1 - i * ELEMENT_LENGTH: -(ELEMENT_LENGTH - 1)];
+            first_rows[i+1] = In1_Copy[FIRST_IN_LENGTH - 1 - i * NUM_FIRST_COL * ELEMENT_LENGTH:FIRST_IN_LENGTH - 1 - i * NUM_FIRST_COL * ELEMENT_LENGTH -(NUM_FIRST_COL*ELEMENT_LENGTH - 1)];
         end
         mux #(.WIDTH(32),.NUM_INPUTS(NUM_FIRST_COL)) mux_i(
                 .In(first_rows[i+1]),
@@ -104,11 +105,11 @@ generate
     end
 endgenerate
 generate
-    for(j=0; j<NUM_SECOND_ROW; j=j+1) begin
+    for(j=0; j<NUM_SECOND_COL; j=j+1) begin
         always @* begin
-            second_cols[j+1] = In2_Copy[SECOND_IN_LENGTH - 1 - j * ELEMENT_LENGTH: -(ELEMENT_LENGTH - 1)];
+            second_cols[j+1] = In2_Copy[SECOND_IN_LENGTH - 1 - j * NUM_SECOND_ROW * ELEMENT_LENGTH:SECOND_IN_LENGTH - 1 - j * NUM_SECOND_ROW * ELEMENT_LENGTH -(NUM_SECOND_ROW*ELEMENT_LENGTH - 1)];
         end
-        mux #(.WIDTH(32),.NUM_INPUTS(NUM_FIRST_COL)) mux_j(
+        mux #(.WIDTH(32),.NUM_INPUTS(NUM_SECOND_ROW)) mux_j(
                 .In(second_cols[j+1]),
                 .select(select_signal),
                 .Out(out_cols_mux[j+1]));
@@ -138,21 +139,30 @@ generate
                 .result_ack(res_ack),
                 .Result(temp2_matrix[i][j]),
                 .result_ready(res_ready[i][j]));
-            always @* begin
-                in1_full_ack = in1_full_ack & in1_ack[i][j];
-                in2_full_ack = in2_full_ack & in2_ack[i][j];
-                output_full_stb = output_full_stb & output_stb[i][j];
-                res_full_ready = res_full_ready & res_ready[i][j];
-            end
         end
     end
 endgenerate
+always @* 
+begin
+    in1_full_ack = 1;
+    in2_full_ack = 1;
+    output_full_stb = 1;
+    res_full_ready = 1;
+    for(n=1; n<= NUM_FIRST_ROW; n=n+1) begin
+        for(m=1; m<=NUM_SECOND_COL; m=m+1) begin
+                in1_full_ack = in1_full_ack & in1_ack[n][m];
+                in2_full_ack = in2_full_ack & in2_ack[n][m];
+                output_full_stb = output_full_stb & output_stb[n][m];
+                res_full_ready = res_full_ready & res_ready[n][m];
+         end
+    end
+ end
 always @(posedge clk, negedge rst) begin
     if (!rst) begin
         state <= get_input_s;
         In1_Copy <= 0;
         In2_Copy <= 0;
-        rst_mult <= 0;
+        rst_mult <= 1;
         reset_adder <= 0;
         select_signal <= 0;
         for(g=1; g<=NUM_FINAL_ROW; g=g+1) begin
@@ -168,7 +178,7 @@ always @(posedge clk, negedge rst) begin
                        In1_Copy <= In1;
                        In2_Copy <= In2;
                        state <= mult_elements_s;
-                       rst_mult <= 1;
+                       rst_mult <= 0;
                     end
                     else begin
                         for(g=1; g<=NUM_FINAL_ROW; g=g+1) begin
@@ -177,7 +187,7 @@ always @(posedge clk, negedge rst) begin
                             end
                         end
                         state <= get_input_s;
-                        rst_mult <= 0;
+                        rst_mult <= 1;
                         select_signal <= 0;
                         output_ack <= 0;
                         res_ack <= 0;
@@ -188,15 +198,15 @@ always @(posedge clk, negedge rst) begin
                 end
             mult_elements_s: begin
                     
-                    if(in1_full_ack && in2_full_ack) begin
+//                    if(in1_full_ack && in2_full_ack) begin
                         in1_stb <= 1;
                         in2_stb <= 1;
                         output_ack <= 0;
                         state <= wait_for_mult_s;
-                    end
-                    else begin
-                        state <= mult_elements_s;
-                    end
+//                    end
+//                    else begin
+//                        state <= mult_elements_s;
+//                    end
                 end
             wait_for_mult_s: begin
                    if(output_full_stb) begin
